9046bc4c08e7d37195293afe1a7ac88a1fb2d4c6
Musa Mahmood <musasmahmood@gmail.com>
2022-03-28 00:55:33 -0400

    Added first three rows with commands and icons.

--- app/src/main/java/com/mmahmood/remotehotkey/AppConstant.kt
+++ app/src/main/java/com/mmahmood/remotehotkey/AppConstant.kt
@@ -19,22 +19,22 @@ class AppConstant {
             HotkeyData("Pycharm", "pycharm", byteArrayOf(0xA2.toByte(), 0x13)),
             HotkeyData("MATLAB", "matlab", byteArrayOf(0xA2.toByte(), 0x10)),
             HotkeyData("Visual Studio", "vstudio", byteArrayOf(0xA2.toByte(), 0x19)), //16
-            HotkeyData("", "", byteArrayOf(0xA2.toByte(), 0x57.toByte())),
-            HotkeyData("", "", byteArrayOf(0xE2.toByte(), 0x11.toByte())),
-            HotkeyData("", "", byteArrayOf(0xC2.toByte(), 0x19.toByte())),
-            HotkeyData("", "", byteArrayOf(0xA2.toByte(), 0x09.toByte())), //20
-            HotkeyData("", "", byteArrayOf(0xE2.toByte(), 0x2D.toByte())),
-            HotkeyData("", "", byteArrayOf(0xE2.toByte(), 0x1E.toByte())),
-            HotkeyData("", "", byteArrayOf(0xE2.toByte(), 0x2E.toByte())),
-            HotkeyData("", "", byteArrayOf(0xA2.toByte(), 0x17.toByte())), //24
-            HotkeyData("", "", byteArrayOf(0x00, 0x00)),
-            HotkeyData("", "", byteArrayOf(0x00, 0x00)),
-            HotkeyData("", "", byteArrayOf(0x00, 0x00)),
-            HotkeyData("", "", byteArrayOf(0x00, 0x00)), //28
-            HotkeyData("", "", byteArrayOf(0x00, 0x00)),
-            HotkeyData("", "", byteArrayOf(0x00, 0x00)),
-            HotkeyData("", "", byteArrayOf(0x00, 0x00)),
-            HotkeyData("", "", byteArrayOf(0x00, 0x00)), //32
+            HotkeyData("Sublime", "subl", byteArrayOf(0xA2.toByte(), 0x57.toByte())),
+            HotkeyData("Android Projects", "android", byteArrayOf(0xE2.toByte(), 0x11.toByte())),
+            HotkeyData("Jlink RTT", "jlink", byteArrayOf(0xC2.toByte(), 0x19.toByte())),
+            HotkeyData("Flutter Projects", "flutter", byteArrayOf(0xA2.toByte(), 0x09.toByte())), //20
+            HotkeyData("Windows Projects", "w11", byteArrayOf(0xE2.toByte(), 0x2D.toByte())),
+            HotkeyData("Python Projects", "python", byteArrayOf(0xE2.toByte(), 0x1E.toByte())),
+            HotkeyData("Linux Projects", "linux", byteArrayOf(0xE2.toByte(), 0x2E.toByte())),
+            HotkeyData("TeamViewer", "teamv", byteArrayOf(0xA2.toByte(), 0x17.toByte())), //24
+            HotkeyData("", "", byteArrayOf(0xE2.toByte(), 0x19.toByte())),
+            HotkeyData("", "", byteArrayOf(0xE2.toByte(), 0x13.toByte())),
+            HotkeyData("", "", byteArrayOf(0x52.toByte(), 0x09.toByte())),
+            HotkeyData("", "", byteArrayOf(0x92.toByte(), 0x17.toByte())), //28
+            HotkeyData("", "", byteArrayOf(0xE2.toByte(), 0x06.toByte())),
+            HotkeyData("", "", byteArrayOf(0xE2.toByte(), 0x10.toByte())),
+            HotkeyData("", "", byteArrayOf(0xA2.toByte(), 0x18.toByte())),
+            HotkeyData("", "", byteArrayOf(0xA2.toByte(), 0x62.toByte())), //32
             HotkeyData("", "", byteArrayOf(0x00, 0x00)),
             HotkeyData("", "", byteArrayOf(0x00, 0x00)),
             HotkeyData("", "", byteArrayOf(0x00, 0x00)),
